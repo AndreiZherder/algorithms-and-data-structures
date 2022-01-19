30cb31b18b5363fcf5dfce78f73fc99b7210f0e1
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-01-19 17:11:15 +0300

    check binary search tree (completed with recursion)

--- 14.4-check-binary-search-tree-1.py
+++ 14.4-check-binary-search-tree-1.py
@@ -91,6 +91,7 @@ CORRECT
 Выход:
 INCORRECT
 """
+import sys
 from collections import namedtuple
 from typing import List
 
@@ -101,21 +102,35 @@ class Tree:
     def __init__(self, nodes: List[Node]):
         self.nodes = nodes
 
-    def check(self, i: int) -> tuple[bool, int, int]:
+    def check(self, i: int) -> (bool, int, int):
+        if not self.nodes:
+            return True, 0, 0
         left_maximum = -2 ** 31
+        left_minimum = 2 ** 31 - 1
+        right_maximum = -2 ** 31
         right_minimum = 2 ** 31 - 1
         if self.nodes[i].left != -1:
             res = self.check(self.nodes[i].left)
-        return (left_maximum < self.nodes[i].key < right_minimum, left_maximum, right_minimum)
-
-
-
+            if not res[0] or res[1] >= self.nodes[i].key:
+                return False, 0, 0
+            left_maximum = res[1]
+            left_minimum = res[2]
+        if self.nodes[i].right != -1:
+            res = self.check(self.nodes[i].right)
+            if not res[0] or res[2] <= self.nodes[i].key:
+                return False, 0, 0
+            right_maximum = res[1]
+            right_minimum = res[2]
+        maximum = max(left_maximum, right_maximum, self.nodes[i].key)
+        minimum = min(left_minimum, right_minimum, self.nodes[i].key)
+        return True, maximum, minimum
 
 
 def main():
+    sys.setrecursionlimit(100000)
     n = int(input())
     tree = Tree([Node(*map(int, input().split())) for _ in range(n)])
-    print('CORRECT' if tree.check(0) else 'INCORRECT')
+    print('CORRECT' if tree.check(0)[0] else 'INCORRECT')
 
 
 if __name__ == '__main__':
