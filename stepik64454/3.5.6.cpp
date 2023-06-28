2cdaf199b262b975ba6f42f2f4d39e105a1d006b
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-06-28 21:42:32 +0300

    stepik64454 - 3.5.6

---
+++ stepik64454/3.5.6.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> cache(301, vector <int> (10001, 0));
+vector<vector<bool>> has_cache(301, vector <bool> (10001, 0));
+
+int dp(int i, int total, int s, int n, vector<int>& w, vector<int>& c) {
+    if (i == n) {
+        return 0;
+    }
+    if (has_cache[i][total]){
+        return cache[i][total];
+    }
+    int ans = dp(i + 1, total, s, n, w, c);
+    if (total + w[i] <= s) {
+        ans = max(ans, dp(i + 1, total + w[i], s, n, w, c) + c[i]);
+    }
+    has_cache[i][total] = true;
+    cache[i][total] = ans;
+    return ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    while (t--) {
+        int s, n;
+        cin >> s >> n;
+        vector<int> w(n), c(n);
+        for (int i = 0; i < n; i++) {
+            cin >> w[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> c[i];
+        }
+        int ans = dp(0, 0, s, n, w, c);
+        vector <int> path;
+        int total = 0;
+        for (int i = 0; i < n; i++){
+            if (total + w[i] <= s and cache[i + 1][total + w[i]] + c[i]> cache[i + 1][total]){
+                path.push_back(i + 1);
+                total += w[i];
+            }
+        }
+        cout << ans << " " << path.size() << endl;
+        for (auto x: path){
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
